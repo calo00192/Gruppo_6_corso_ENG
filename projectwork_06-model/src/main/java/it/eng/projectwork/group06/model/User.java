0bb4862bd4051e3962ec9ef719ff3f69e2c78b1b
Calogero <Calogero@Gero-PC>
2018-02-20 17:13:55 +0100

    2018_02_20_17_13

--- projectwork_06-model/src/main/java/it/eng/projectwork/group06/model/User.java
+++ projectwork_06-model/src/main/java/it/eng/projectwork/group06/model/User.java
@@ -8,4 +8,68 @@ public class User {
 
 	@Id
 	private String username;
+	
+	private String name;
+	
+	private String surname;
+	
+	private String emailAddress;
+	
+	public User() {}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	/**
+	 * @param emailAddress the emailAddress to set
+	 */
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
 }
